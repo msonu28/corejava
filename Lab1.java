231cd4870160e45699ad22a413c270f4277645b8
sonu <msonu.sit4@gmail.com>
2019-07-09 14:29:58 +0530

    add Lab1 file in local

---
+++ Lab1.java
@@ -0,0 +1,12 @@
+class Lab1{
+  public static void main(String[] args){
+    int a = 10;
+    int b = 20;
+    int c = 30
+    int d = 100;
+    System.out.println(a+b);
+    System.out.println(a+b+c);
+    System.out.println(a+b+c+d);
+    System.out.println("Hi My name is Sonu");
+  }
+}
